8a49aaa38649dd4072475a166ea60036b95e9be0
Arka Roy <i.arkaroy@gmail.com>
2018-04-17 13:04:33 +0530

    Added render

--- src/js/main.js
+++ src/js/main.js
@@ -3,37 +3,49 @@ require('../scss/main.scss');
 var document = window.document;
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-const SEGMENTS = 6;
+const SEGMENTS = 8;
 const DEGREE = 10;
+const GAP = 2;
+var pieces = [];
 
 var img = new Image();
+img.crossOrigin = "anonymous";
 img.onload = function () {
     var cv = simRender(img);
     var w = cv.width,
         h = cv.height;
-    console.log(w, h);
-    /*
-    var w = img.naturalWidth,
-        h = img.naturalHeight;
     var s = w / SEGMENTS;
-    var cv = document.createElement('canvas');
-    var cx = cv.getContext('2d');
-    cv.width = w;
-    cv.height = h;
-    document.body.appendChild(cv);
-    cx.rotate(DEGREE * Math.PI / 180);
+    pieces = [];
     for (var i = 0; i < SEGMENTS; ++i) {
         var p = s * i;
-        console.log(s, p);
-        cx.drawImage(img, p, 0, s, h, p, 0, s, h);
+        var cx = context();
+        cx.canvas.width = s;
+        cx.canvas.height = h;
+        cx.drawImage(cv, p, 0, s, h, 0, 0, s, h);
+        pieces.push(cx.canvas);
     }
-    cx.setTransform(1, 0, 0, 1, 0, 0);
-    */
+    render(w);
 };
-img.src = 'https://cdn.allwallpaper.in/wallpapers/480x272/646/landscapes-valley-fantasy-art-artwork-480x272-wallpaper.jpg';
+img.src = 'http://localhost:8080/art.jpg';
+
+function render(width) {
+    var totalWidth = width + (SEGMENTS - 1) * GAP;
+    ctx.save();
+    ctx.translate(canvas.width / 2, canvas.height / 2);
+    //ctx.rotate(DEGREE * Math.PI / 180);
+    for (var i = 0; i < SEGMENTS; ++i) {
+        var p = pieces[i];
+        p.dstX = (i * p.width + GAP * i) - totalWidth / 2;
+        p.dstY = -p.height / 2;
+        ctx.drawImage(p, p.dstX, p.dstY);
+        ctx.strokeRect(p.dstX, p.dstY, p.width, p.height);
+    }
+    console.log(pieces);
+    ctx.restore();
+}
 
 function simRender(img) {
     var cx = context();
@@ -47,7 +59,6 @@ function simRender(img) {
     var nw = w * cos + h * sin;
     cx.canvas.width = nw;
     cx.canvas.height = nh;
-    document.body.appendChild(cx.canvas);
     cx.save();
     cx.translate(cx.canvas.width / 2, cx.canvas.height / 2);
     cx.rotate(deg * Math.PI / 180);
