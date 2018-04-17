8b6213a6c697d0cc5fa695947c61ee5184faed60
Arka Roy <i.arkaroy@gmail.com>
2018-04-17 12:08:48 +0530

    Added ghost render

--- src/js/main.js
+++ src/js/main.js
@@ -7,9 +7,15 @@ var ctx = canvas.getContext('2d');
 // canvas.height = window.innerHeight;
 
 const SEGMENTS = 6;
+const DEGREE = 10;
 
 var img = new Image();
 img.onload = function () {
+    var cv = simRender(img);
+    var w = cv.width,
+        h = cv.height;
+    console.log(w, h);
+    /*
     var w = img.naturalWidth,
         h = img.naturalHeight;
     var s = w / SEGMENTS;
@@ -18,12 +24,38 @@ img.onload = function () {
     cv.width = w;
     cv.height = h;
     document.body.appendChild(cv);
-    // ctx.rotate(10 * Math.PI / 180);
+    cx.rotate(DEGREE * Math.PI / 180);
     for (var i = 0; i < SEGMENTS; ++i) {
         var p = s * i;
         console.log(s, p);
         cx.drawImage(img, p, 0, s, h, p, 0, s, h);
     }
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    cx.setTransform(1, 0, 0, 1, 0, 0);
+    */
 };
-img.src = 'https://cdn.allwallpaper.in/wallpapers/640x480/646/landscapes-valley-fantasy-art-artwork-640x480-wallpaper.jpg';
+img.src = 'https://cdn.allwallpaper.in/wallpapers/480x272/646/landscapes-valley-fantasy-art-artwork-480x272-wallpaper.jpg';
+
+function simRender(img) {
+    var cx = context();
+    var w = img.naturalWidth,
+        h = img.naturalHeight;
+    var deg = DEGREE * -1,
+        rad = deg * Math.PI / 180,
+        sin = Math.abs(Math.sin(rad)),
+        cos = Math.abs(Math.cos(rad));
+    var nh = h * cos + w * sin;
+    var nw = w * cos + h * sin;
+    cx.canvas.width = nw;
+    cx.canvas.height = nh;
+    document.body.appendChild(cx.canvas);
+    cx.save();
+    cx.translate(cx.canvas.width / 2, cx.canvas.height / 2);
+    cx.rotate(deg * Math.PI / 180);
+    cx.drawImage(img, -w / 2, -h / 2);
+    cx.restore();
+    return cx.canvas;
+}
+
+function context() {
+    return document.createElement('canvas').getContext('2d');
+}
