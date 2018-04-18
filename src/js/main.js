e9ead4cef591e2e9796aad922fa92b6787924ff6
Arka Roy <i.arkaroy@gmail.com>
2018-04-18 11:24:19 +0530

    Adding method for slicing text

--- src/js/main.js
+++ src/js/main.js
@@ -6,19 +6,44 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const SEGMENTS = 8;
-const DEGREE = 10;
-const GAP = 2;
+const SEGMENTS = 40;
+const DEGREE = 30;
+const GAP = 1;
 var pieces = [];
 
-var img = new Image();
-img.crossOrigin = "anonymous";
-img.onload = function () {
-    var cv = initRender(img);
+//sliceImage('http://localhost:8080/art.jpg');
+sliceText('Demo Text');
+
+function sliceImage(src) {
+    var img = new Image();
+    img.crossOrigin = "anonymous";
+    img.onload = function () {
+        var cv = initRender(img);
+        initPieces(cv);
+        requestAnimationFrame(render);
+    };
+    img.src = src;
+}
+
+function sliceText(text) {
+    var cx = context();
+    var font = `bold 80px arial`;
+    var span = document.createElement('span');
+    span.textContent = text;
+    span.style.font = font;
+    document.body.appendChild(span);
+    var w = span.offsetWidth,
+        h = span.offsetHeight;
+    cx.canvas.width = w;
+    cx.canvas.height = h;
+    cx.font = font;
+    cx.textBaseline = 'middle';
+    cx.textAlign = 'center';
+    cx.fillText(text, w / 2, h / 2);
+    var cv = initRender(cx.canvas);
     initPieces(cv);
     requestAnimationFrame(render);
-};
-img.src = 'http://localhost:8080/art.jpg';
+}
 
 function render() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -27,14 +52,14 @@ function render() {
     ctx.rotate(DEGREE * Math.PI / 180);
     for (var i = 0; i < SEGMENTS; ++i) {
         var p = pieces[i];
-        if(p.wait <= 0) {
+        if (p.wait <= 0) {
             p.currX += (p.dstX - p.currX) * 0.04;
             p.currY += (p.dstY - p.currY) * 0.04;
         } else {
             p.wait--;
         }
         ctx.drawImage(p, p.currX, p.currY);
-        // ctx.strokeRect(p.currX, p.currY, p.width, p.height);
+        //ctx.strokeRect(p.currX, p.currY, p.width, p.height);
     }
     ctx.restore();
     requestAnimationFrame(render);
@@ -57,7 +82,7 @@ function initPieces(cvs) {
         cx.canvas.currX = cx.canvas.dstX;
         var ys = [
             canvas.height / 2,
-            -(canvas.height/2 + cx.canvas.height)
+            -(canvas.height / 2 + cx.canvas.height)
         ];
         cx.canvas.currY = ys[Math.floor(Math.random() * ys.length)];
         cx.canvas.wait = Math.floor(Math.random() * 50);
