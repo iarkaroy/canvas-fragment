edc4eabc43b4609c56dad6efcfbdeb0e615d17a0
Arka Roy <i.arkaroy@gmail.com>
2018-04-19 12:56:49 +0530

    Implemented diagonal slicing with clip

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,13 @@ require('../scss/main.scss');
 
 var document = window.document;
 var canvas = document.getElementById('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var SEGMENTS = 20;
-const DEGREE = 45;
+const SEGMENTS = 10;
+const DEGREE = -45;
 const GAP = 0;
 var pieces = [];
 
@@ -21,9 +19,8 @@ function sliceImage(src) {
     var img = new Image();
     img.crossOrigin = "anonymous";
     img.onload = function () {
-        var cv = initRender(img);
-        // initPieces(cv);
-        // requestAnimationFrame(render);
+        initRender(img);
+        requestAnimationFrame(render);
     };
     img.src = src;
 }
@@ -37,24 +34,21 @@ function sliceText(text) {
     document.body.appendChild(span);
     var w = span.offsetWidth,
         h = span.offsetHeight;
+    document.body.removeChild(span);
     cx.canvas.width = w;
     cx.canvas.height = h;
     cx.font = font;
     cx.textBaseline = 'middle';
     cx.textAlign = 'center';
     cx.fillText(text, w / 2, h / 2);
-    SEGMENTS = Math.ceil(w / 20);
-    var cv = initRender(cx.canvas);
-    initPieces(cv);
+    initRender(cx.canvas);
     requestAnimationFrame(render);
 }
 
 function render() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.save();
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate(DEGREE * Math.PI / 180);
-    for (var i = 0; i < SEGMENTS; ++i) {
+    var len = pieces.length;
+    for (var i = 0; i < len; ++i) {
         var p = pieces[i];
         if (p.wait <= 0) {
             p.currX += (p.dstX - p.currX) * 0.04;
@@ -62,72 +56,63 @@ function render() {
         } else {
             p.wait--;
         }
-        ctx.drawImage(p, p.dstX, p.dstY);
-        //ctx.strokeRect(p.dstX, p.dstY, p.width, p.height);
+        ctx.drawImage(p, p.currX, p.currY);
     }
-    ctx.restore();
     requestAnimationFrame(render);
 }
 
-function initPieces(cvs) {
-    var w = cvs.width,
-        h = cvs.height;
-    var s = w / SEGMENTS;
-    pieces = [];
-    var totalWidth = w + (SEGMENTS - 1) * GAP;
-    for (var i = 0; i < SEGMENTS; ++i) {
-        var p = s * i;
-        var cx = context();
-        cx.canvas.width = s;
-        cx.canvas.height = h;
-        cx.drawImage(cvs, p, 0, s, h, 0, 0, s, h);
-        cx.canvas.dstX = (i * cx.canvas.width + GAP * i) - totalWidth / 2;
-        cx.canvas.dstY = -cx.canvas.height / 2;
-        cx.canvas.currX = cx.canvas.dstX;
-        var ys = [
-            canvas.height / 2,
-            -(canvas.height / 2 + cx.canvas.height)
-        ];
-        cx.canvas.currY = ys[Math.floor(Math.random() * ys.length)];
-        cx.canvas.wait = Math.floor(Math.random() * 50);
-        pieces.push(cx.canvas);
-    }
-}
-
 function initRender(image) {
-    var w = image.width,
-        h = image.height;
-    console.log(w, h);
-    var ctx = context();
-    ctx.canvas.width = w;
-    ctx.canvas.height = h;
-    ctx.save();
-    ctx.rotate(45 * Math.PI / 180)
-    ctx.rect(0, 0, w, 15);
-    ctx.restore();
-    ctx.clip();
-    ctx.drawImage(image, 0, 0);
-    document.body.appendChild(ctx.canvas);
-}
+    var w, h, rad, sin, cos, nw, nh, sh, i;
 
-function initRender1(img) {
-    var cx = context();
-    var w = img.width,
-        h = img.height;
-    var deg = DEGREE * -1,
-        rad = deg * Math.PI / 180,
-        sin = Math.abs(Math.sin(rad)),
-        cos = Math.abs(Math.cos(rad));
-    var nh = h * cos + w * sin;
-    var nw = w * cos + h * sin;
-    cx.canvas.width = nw;
-    cx.canvas.height = nh;
-    cx.save();
-    cx.translate(cx.canvas.width / 2, cx.canvas.height / 2);
-    cx.rotate(deg * Math.PI / 180);
-    cx.drawImage(img, -w / 2, -h / 2);
-    cx.restore();
-    return cx.canvas;
+    // Dimension of image
+    w = image.width;
+    h = image.height;
+
+    // Convert degree to radian
+    rad = DEGREE * Math.PI / 180;
+
+    // Sine and cosine of radian
+    sin = Math.abs(Math.sin(rad));
+    cos = Math.abs(Math.cos(rad));
+
+    // Dimension of rotated image
+    nw = w * cos + h * sin;
+    nh = h * cos + w * sin;
+
+    // Height of per piece
+    sh = nh / SEGMENTS;
+
+    pieces = [];
+    for (i = 0; i < SEGMENTS; ++i) {
+        var ctx = context();
+        ctx.canvas.width = w;
+        ctx.canvas.height = h;
+        ctx.save();
+        ctx.translate(w / 2, h / 2);
+        ctx.rotate(DEGREE * Math.PI / 180);
+        ctx.rect(-nw / 2, sh * i - nh / 2, nw, sh);
+        ctx.restore();
+        ctx.clip();
+        ctx.drawImage(image, 0, 0);
+        ctx.canvas.dstX = (canvas.width - w) / 2;
+        ctx.canvas.dstY = (canvas.height - h) / 2;
+        var ys = [
+            -h,
+            canvas.height
+        ];
+        ctx.canvas.currY = ys[Math.floor(Math.random() * ys.length)];
+        if (ctx.canvas.currY < 0) {
+            var dg = 90 - Math.atan2(ctx.canvas.dstY - ctx.canvas.currY, 1) * 180 / Math.PI;
+            var d = 1 / dg * DEGREE;
+            ctx.canvas.currX = ctx.canvas.dstX - d;
+        } else {
+            var dg = 90 + Math.atan2(ctx.canvas.dstY - ctx.canvas.currY, 1) * 180 / Math.PI;
+            var d = 1 / dg * DEGREE;
+            ctx.canvas.currX = ctx.canvas.dstX + d;
+        }
+        ctx.canvas.wait = Math.floor(Math.random() * 50);
+        pieces.push(ctx.canvas);
+    }
 }
 
 function context() {
